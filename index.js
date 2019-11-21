03ed8f4a30fad6b628ffcf02296eb45f880efcf6
Karl Swedberg <kswedberg@gmail.com>
2019-11-21 09:29:28 -0500

    Bump ecmaVersion to 2019

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const rules = Object.assign(
 
 module.exports = {
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2019,
     sourceType: 'module',
     ecmaFeatures: {
       impliedStrict: true,
