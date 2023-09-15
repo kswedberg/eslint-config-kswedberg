0f79bbbfef745b0c781155dd98ab480bea064148
Karl Swedberg <kswedberg@gmail.com>
2023-09-15 11:36:43 -0400

    Add promise and export rules

---
+++ import.js
@@ -0,0 +1,8 @@
+module.exports = {
+  rules: {
+    'import/order': 'warn',
+    'import/first': 'warn',
+    'import/no-mutable-exports': 'warn',
+    'import/no-unresolved': 'off',
+  },
+};
---
+++ promise.js
@@ -0,0 +1,12 @@
+module.exports = {
+  rules: {
+    'promise/always-return': 'warn',
+    'promise/no-return-wrap': 'warn',
+    'promise/param-names': 'warn',
+    'promise/catch-or-return': 'warn',
+    'promise/no-callback-in-promise': 'warn',
+    'promise/no-new-statics': 'warn',
+    'promise/no-return-in-finally': 'warn',
+    'promise/valid-params': 'warn',
+  },
+};
